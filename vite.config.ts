bdc661630f8ff05b0a38d02bbcd8d3585fba6c30
niksedov24 <nik.sedov890@gmail.com>
2026-04-24 13:03:06 +0400

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Tlt-practice/", // ЗАМЕНИ на имя твоего репозитория
 
   server: {
     open: true,
